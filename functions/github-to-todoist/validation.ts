4672f8a2fcc84c73b4e89bbd7daa433ecf84bdfc
James Camilleri <james@james.mt>
2023-10-18 23:45:35 +0100

    Remove logs

--- functions/github-to-todoist/validation.ts
+++ functions/github-to-todoist/validation.ts
@@ -4,26 +4,13 @@ export async function verifyGithubEvent(request: Request, secret: string) {
   const requestSignature = request.headers.get('x-hub-signature-256')
   const body = await request.text()
 
-  console.debug('requestSignature', requestSignature)
-  console.debug('secret', secret)
-
   if (!requestSignature) {
     return
   }
 
   const signature = crypto.createHmac('sha256', secret).update(body).digest('hex')
-
-  console.debug('signature', signature)
-
   const trusted = Buffer.from(`sha256=${signature}`, 'ascii')
   const untrusted = Buffer.from(requestSignature, 'ascii')
-
-  console.debug('trusted', trusted)
-  console.debug('untrusted', untrusted)
-  console.debug(
-    'crypto.timingSafeEqual(trusted, untrusted)',
-    crypto.timingSafeEqual(trusted, untrusted),
-  )
 
   if (crypto.timingSafeEqual(trusted, untrusted)) {
     return JSON.parse(body)
