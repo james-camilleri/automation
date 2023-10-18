0226b9297fb1c67147bd8e5007f631e1c5a4b702
James Camilleri <james@james.mt>
2023-10-18 23:23:46 +0100

    Add logs to validation for testing

--- functions/github-to-todoist/validation.ts
+++ functions/github-to-todoist/validation.ts
@@ -2,6 +2,9 @@ import * as crypto from 'crypto'
 
 export const verifySignature = (request: Request, secret: string) => {
   const requestSignature = request.headers.get('x-hub-signature-256')
+  console.debug('requestSignature', requestSignature)
+  console.debug('secret', secret)
+
   if (!requestSignature) {
     return false
   }
@@ -11,8 +14,17 @@ export const verifySignature = (request: Request, secret: string) => {
     .update(JSON.stringify(request.body))
     .digest('hex')
 
+  console.debug('signature', signature)
+
   const trusted = Buffer.from(`sha256=${signature}`, 'ascii')
   const untrusted = Buffer.from(requestSignature, 'ascii')
+
+  console.debug('trusted', trusted)
+  console.debug('untrusted', untrusted)
+  console.debug(
+    'crypto.timingSafeEqual(trusted, untrusted)',
+    crypto.timingSafeEqual(trusted, untrusted),
+  )
 
   return crypto.timingSafeEqual(trusted, untrusted)
 }
